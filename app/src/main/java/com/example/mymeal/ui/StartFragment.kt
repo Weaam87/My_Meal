8509d41cb4bed3446da9633ea99299203f171928
Weaam Alagha <alwassof25@gmail.com>
2022-07-05 04:23:26 +0800

    Different changes

---
+++ app/src/main/java/com/example/mymeal/ui/StartFragment.kt
@@ -0,0 +1,20 @@
+package com.example.mymeal.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mymeal.R
+
+class StartFragment : Fragment() {
+
+   // private val _binding :
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+}
