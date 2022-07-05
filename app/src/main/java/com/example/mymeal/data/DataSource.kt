abf8fdc6fe685fb39a106eebce6e089a321a3ce6
Weaam Alagha <alwassof25@gmail.com>
2022-07-05 18:02:17 +0800

    Edit DataSource.kt

--- app/src/main/java/com/example/mymeal/data/DataSource.kt
+++ app/src/main/java/com/example/mymeal/data/DataSource.kt
@@ -36,42 +36,6 @@ object DataSource {
                     description = "3 skewers of chicken breast chunks with our special seasoning."+
                             " Served with fries, spicy tomato sauce, yogurt and hummus",
                     price = 32.5
-                ),
-        "soup" to
-                MenuItem(
-                    name = "Butternut Squash Soup",
-                    description = "Roasted butternut squash, roasted peppers, chili oil",
-                    price = 3.00
-                ),
-        "potatoes" to
-                MenuItem(
-                    name = "Spicy Potatoes",
-                    description = "Marble potatoes, roasted, and fried in house spice blend",
-                    price = 2.00
-                ),
-        "rice" to
-                MenuItem(
-                    name = "Coconut Rice",
-                    description = "Rice, coconut milk, lime, and sugar",
-                    price = 1.50
-                ),
-        "bread" to
-                MenuItem(
-                    name = "Lunch Roll",
-                    description = "Fresh baked roll made in house",
-                    price = 0.50
-                ),
-        "berries" to
-                MenuItem(
-                    name = "Mixed Berries",
-                    description = "Strawberries, blueberries, raspberries, and huckleberries",
-                    price = 1.00
-                ),
-        "pickles" to
-                MenuItem(
-                    name = "Pickled Veggies",
-                    description = "Pickled cucumbers and carrots, made in house",
-                    price = 0.50
                 )
     )
 }
