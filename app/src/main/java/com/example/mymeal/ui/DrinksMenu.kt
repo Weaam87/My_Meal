b74f749c3127ac9e7ae246e11374fce787a355b1
Weaam Alagha <alwassof25@gmail.com>
2022-07-06 23:30:32 +0800

    Implement DrinksMenu.kt & fragment_drinks_menu.xml

--- app/src/main/java/com/example/mymeal/ui/DrinksMenu.kt
+++ app/src/main/java/com/example/mymeal/ui/DrinksMenu.kt
@@ -5,56 +5,69 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.mymeal.R
+import com.example.mymeal.databinding.FragmentDrinksMenuBinding
+import com.example.mymeal.model.OrderViewModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DrinksMenu.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DrinksMenu : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // Binding object instance corresponding to the fragment_drinks_menu.xml layout.
+    private var _binding: FragmentDrinksMenuBinding? = null
+
+    private val binding get() = _binding!!
+
+    //Get instance of OrderViewModel
+    private val sharedViewModel: OrderViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_drinks_menu, container, false)
+        _binding = FragmentDrinksMenuBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DrinksMenu.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DrinksMenu().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = sharedViewModel
+        binding.drinkMenu = this
+    }
+
+    //Navigate to the desserts menu fragment
+    fun goToNextFragment() {
+        findNavController().navigate(R.id.action_drinksMenu_to_dessertsMenu)
+    }
+
+    //Cancel the order
+    private fun cancelOrder() {
+        //Reset the values
+        sharedViewModel.resetOrder()
+        //Navigate back to the start fragment
+        findNavController().navigate(R.id.action_drinksMenu_to_startFragment)
+    }
+
+    private fun doNothing() {
+        return
+    }
+
+    //confirmation before cancel the order
+    fun showDialog() {
+        MaterialAlertDialogBuilder(requireContext()).setMessage(getString(R.string.cancel_order))
+            .setCancelable(true)
+            .setNegativeButton(getString(R.string.yes)) { _, _ -> cancelOrder() }
+            .setPositiveButton(getString(R.string.no)) { _, _ -> doNothing() }.show()
+    }
+
+    /**
+    Clear out the binding object when the view hierarchy associated with the fragment
+    is being removed
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
