34014c2aa2c621b646b9cb37387906ce5dedf37d
Weaam Alagha <alwassof25@gmail.com>
2022-07-27 11:33:01 +0800

    some fixes

--- app/src/main/java/com/example/mymeal/MainActivity.kt
+++ app/src/main/java/com/example/mymeal/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.mymeal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
@@ -16,8 +15,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var navController: NavController
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
-
-    private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,22 +25,16 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         navController = navHostFragment.navController
         navigationView = findViewById(R.id.navigationView)
         drawerLayout = findViewById(R.id.drawer_layout)
-        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
 
         navigationView.setNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.order_history_drawer -> if (navController.currentDestination?.id == R.id.startFragment) {
-                    navController.navigate(R.id.action_startFragment_to_orderListFragment)
-                }
-                R.id.start_new_order -> if (navController.currentDestination?.id == R.id.orderListFragment) {
-                    navController.navigate(R.id.action_orderListFragment_to_startFragment)
-                }
+                R.id.order_history_drawer -> navController.navigate(R.id.action_startFragment_to_orderListFragment)
+                R.id.start_new_order -> navController.navigate(R.id.action_startFragment_to_mainMeal)
             }
             drawerLayout.closeDrawer(GravityCompat.START)
             true
         }
+
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
